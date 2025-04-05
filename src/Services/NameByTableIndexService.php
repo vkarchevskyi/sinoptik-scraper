7f459ad95a59131f9b862b69f3aabff6b243d259
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-05 11:54:41 +0300

    feat: create NameByTableIndexService

---
+++ src/Services/NameByTableIndexService.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vkarchevskyi\SinoptikUaParser\Services;
+
+class NameByTableIndexService
+{
+    public function get(int $index): string
+    {
+        return match ($index) {
+            0 => 'Picture',
+            1 => 'Temperature',
+            2 => 'Feels like',
+            3 => 'Pressure',
+            4 => 'Humidity',
+            5 => 'Wind (m/s)',
+            6 => 'Probability of Precipitation',
+        };
+    }
+}
